ffcf7e13347b10f7721d38b08ff06aa8d82603d1
fzentenomaier <fzenteno.maier@gmail.com>
2024-05-10 16:22:41 +0800

    Update app_nba.py

--- app_nba.py
+++ app_nba.py
@@ -15,6 +15,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import numpy as np
+import lxml
 
 nba_teams = {
     "Atlanta Hawks": "ATL",

ccef55774bae0cfb71b4fbe815416ae0c032560b
fzentenomaier <fzenteno.maier@gmail.com>
2024-05-29 15:43:30 +0800

    Update app_nba.py

--- app_nba.py
+++ app_nba.py
@@ -406,9 +406,9 @@ def main():
             players_df, stats_df = get_MVP_prediction(playerstats)
             players_df = players_df.head(10)
             # Create the pie chart subplot
-            fig = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
-            fig.add_trace(go.Pie(labels=players_df.Player, values=players_df.MVP_Winner, textinfo='label'),1,1)
-            fig.add_trace(go.Pie(labels=stats_df.Stat, values=stats_df['Importance_%'], textinfo='label'),1,2)
+            fig = make_subplots(rows=1, cols=5, specs=[[{'type':'domain'}, {'type':'domain'}]])
+            fig.add_trace(go.Pie(labels=players_df.Player, values=players_df.MVP_Winner, textinfo='label'),1,2)
+            fig.add_trace(go.Pie(labels=stats_df.Stat, values=stats_df['Importance_%'], textinfo='label'),1,4)
             # Use `hole` to create a donut-like pie chart
             fig.update_traces(hole=.4, hoverinfo="label+percent+name")
             fig.update_layout(showlegend=False,
